346e611d50321b302fcd6c0aaca3eccd3f79b27d
tuyennv <tuyennv@rikkeisoft.com>
2020-06-24 11:11:16 +0700

    update router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Login from './components/login/Login'
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
