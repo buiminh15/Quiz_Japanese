42637615eb8c2d6437a353c6153947177a410a8d
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-06-24 07:28:22 +0700

    add comment

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,13 +1,33 @@
 const authController = require('../controllers/auth.controller')
 const validateToken = require('../middlewares/validateToken');
-const passport = require('passport');
 const router = require('express').Router();
 
-
-// Register Route
+/**
+   * @api {post} api/v1/users/register Create User
+   * @apiDescription Create User
+   * @apiVersion 1.0.0
+   * @apiName Register
+   * @apiGroup Authen-Autho
+   *
+   *
+   * @apiSuccess {Object} Message Register Success
+   *
+   * @apiError (Bad Request 400)  Bad Request  
+   */
    router.post('/register', authController.register);
 
-// Login
+/**
+   * @api {post} api/v1/users/login Login
+   * @apiDescription Login
+   * @apiVersion 1.0.0
+   * @apiName Login
+   * @apiGroup Authen-Autho
+   *
+   *
+   * @apiSuccess {Object} Message Login Success - Token
+   *
+   * @apiError (Bad Request 400)  Bad Request  
+   */
   router.post('/login',  authController.login);
 
 
