ed81f8820dc156ff7bbea77640adb969be1e923c
minh1991 <hikari.mei.119@gmail.com>
2020-06-24 14:14:12 +0700

    layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Login from './components/login/Login'
+import Header_top from './components/layout/Navbar.layout'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header_top/>
         <Switch>
           <Route exact path="/login" component={Login}></Route>
         </Switch>
--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,18 +1,55 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { Carousel } from "react-bootstrap";
 
 class Login extends Component {
-    constructor (props) {
-        super(props)
-        this.state = {}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <div>Login page</div>
-            // <a name="" id="" className="btn btn-primary" role="button">abcsa test</a>
-        )
-    }
+  render() {
+    return (
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/id/237/200/300"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/200/300?grayscale"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/seed/picsum/200/300"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
 }
 
-export default Login
+export default Login;
