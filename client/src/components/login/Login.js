10c3eb7ac6ff4da7ab248c85eb405c695fad5a31
minh1991 <hikari.mei.119@gmail.com>
2020-06-24 10:55:05 +0700

    confix package.json

---
+++ client/src/components/login/Login.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+
+class Login extends Component {
+    render() {
+       <a name="" id="" class="btn btn-primary" href="#" role="button">abcsa test</a>
+    }
+}
